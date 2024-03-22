5f78d001ea56e1b61c7e5ce5c22c8b53c2857700
emanuele-04 <emanuelepelliccia2004@gmail.com>
2024-03-22 01:00:02 +0100

    inserito le card ed il footer

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let navbar = document.querySelector(".navbar")
 
 window.addEventListener("scroll", ()=>{
     console.log(window.scrollY);
-    if (window.scrollY > 0) {
-        navbar.classList.add("nav_scroll")
+    if (window.scrollY > 0 && window.innerWidth > 800) {
+        navbar.classList.add("marginNavbar")
     }else {
-        navbar.classList.remove("nav_scroll")
+        navbar.classList.remove("marginNavbar")
     }
 })
 
@@ -23,7 +23,7 @@ function creatInterval(elementId, finalNumber, frequency) {
             elementId.innerHTML = counter;
         } else {
             clearInterval(intervallo);
-           }
+        }
     }, frequency);
     
 }
@@ -32,3 +32,48 @@ creatInterval(numArticle, 500, 20)
 creatInterval(numUtent, 1000, 10)
 creatInterval(numComment, 300, 40)
 
+
+let annunci = [
+    {name : "articoli", categoria: "giochi", prezzo:"50€", img:"./immagini/giochi-misti.jpg"},
+    {name : "accessori", categoria: "control, tastiere...", prezzo:"150€", img:"./accessori.jpg" },
+    {name : "connessioni", categoria: "ultra-fibra", prezzo:"200€", img:"./immagini/online.png"}
+    
+    
+]
+
+let cardwrapper = document.querySelector("#cardwrapper")
+
+annunci.forEach((annuncio)=>{
+    
+    let col = document.createElement("div")
+    col.classList.add("col-12", "col-lg-4")
+    col.innerHTML = `
+    <div class="card d-flex mt-5 position-relative justify-content-evenly bg-p text-white" data-aos="fade-up">
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-black">
+    NEW
+    </span>
+    <div class="overflow-hidden">
+    <img src=${annuncio.img} class="img_card card-img-top" alt="">
+    
+    </div>
+    <div class="card-bod d-flex flex-column justify-content-between">
+    <div>
+    
+    <h4 class="card-title text-center fw-blod mb-3">${annuncio.name}</h4>
+    <p class" card-text ">categoria: <span class"fs-4 m-0">${annuncio.categoria}</span></p>
+    <p class" card-text ">prezzo: <span class"fs-5">${annuncio.prezzo}</span></p>
+    
+    </div>
+    <div>
+    <i class="bi bi-heart fs-3"></i>
+    <a href="#" class="btn btn-outline-secondary">aggiungi al carello</a>
+    
+    </div>
+    </div>
+    </div>
+    `
+    cardwrapper.appendChild(col)
+
+})
+
+console.log(annunci);
